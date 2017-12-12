ed2ea4a29c52503add56bdbc4f8c3fc8c00ce52e
jm <jhn.meelr@gmail.com>
2017-12-12 17:19:35 +0200

    Add: location search

---
+++ src/components/location-search.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, TextInput, ActivityIndicator, StyleSheet } from 'react-native';
+
+export default class LocationSearch extends React.Component {
+  render() {
+    const { value } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>PICKUP LOCATION</Text>
+        {value && (<TextInput style={styles.location} value={value} />)}
+        {!value && <ActivityIndicator style={styles.spinner} />}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    margin: 20,
+    marginTop: 40,
+    height: 60,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 12,
+    color: 'green',
+    fontWeight: 'bold',
+  },
+    location: {
+    height: 40,
+    textAlign: 'center',
+    fontSize: 13,
+  },
+  spinner: {
+    margin: 10,
+  },
+});
